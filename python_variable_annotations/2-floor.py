fd2f25dfa0d6b7489a414e721c9f8fbc37fa2156
Ali731-Amb <12252@holbertonstudents.com>
2026-04-08 16:37:02 +0200

    docstringTask02

--- python_variable_annotations/2-floor.py
+++ python_variable_annotations/2-floor.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env python3
+"""This module provides a function to get the floor of a float."""
 import math
 
 
 def floor(n: float) -> int:
+    """Return the floor of a floating point number.
+
+    Args:
+        n (float): The number to floor.
+    Returns:
+        int: The largest integer less than or equal to n.
+    """
     return math.floor(n)
