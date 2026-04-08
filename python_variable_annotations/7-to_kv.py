996fd6648cfe00ee41de716531a1513fb05b2f36
Ali731-Amb <12252@holbertonstudents.com>
2026-04-08 16:42:51 +0200

    docstringTask07

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -1,6 +1,17 @@
 #!/usr/bin/env python3
+"""This module provides a function to create a
+key-value tuple with a squared value."""
 from typing import Union, Tuple
 
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """Return a tuple with a string and the
+    square of a number as a float.
+    Args:
+        k (str): The key string.
+        v (Union[int, float]): The value to square.
+    Returns:
+        Tuple[str, float]: A tuple with the string
+        and the squared value as a float.
+    """
     return (k, v ** 2)
