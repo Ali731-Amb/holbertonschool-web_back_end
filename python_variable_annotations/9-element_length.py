9d5762ff97458c6d80a53fd4f365b3d682b336ca
Ali731-Amb <12252@holbertonstudents.com>
2026-04-08 16:46:38 +0200

    docstringTask09

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -1,6 +1,18 @@
 #!/usr/bin/env python3
+"""This module provides a function to return the
+length of each element in an iterable of sequences."""
 from typing import Iterable, Sequence, List, Tuple
 
 
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """Return a list of tuples with each sequence and
+    its length.
+
+    Args:
+        lst (Iterable[Sequence]): An iterable of sequence
+        objects.
+    Returns:
+        List[Tuple[Sequence, int]]: A list of tuples,
+        each containing a sequence and its length.
+    """
     return [(i, len(i)) for i in lst]
--- python_variable_annotations/9-main.py
+++ python_variable_annotations/9-main.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+"""This module tests the element_length function and prints its annotations."""
 
 element_length =  __import__('9-element_length').element_length
 
