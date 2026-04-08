f8f80e3faff9b04f6fa565a471d9dbd715bc106c
Ali731-Amb <12252@holbertonstudents.com>
2026-04-08 16:26:29 +0200

    docstringTask00

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python3
 
 def add(a: float, b: float) -> float:
+    """
+    Add two floating point numbers.
+
+    Args:
+        a (float): The first number to add.
+        b (float): The second number to add.
+
+    Returns:
+        float: The sum of a and b.
+    """
     return a + b
