3fe9cb99033cb1ecff5eb536c5e60dd41102d04c
Ali731-Amb <12252@holbertonstudents.com>
2026-04-20 15:05:02 +0200

    Task00_change_comment_add_import

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
+"""Module for index_range helper function."""
+from typing import Tuple
 
 
-def index_range(page: int, page_size: int) -> tuple:
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """Return a tuple of start and end indexes for a given
+    page and page_size."""
     start = (page - 1) * page_size
     end = start + page_size
     return (start, end)
