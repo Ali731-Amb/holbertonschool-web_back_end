a84aa09fdabe07b2fd6bf52cadac19452d542548
Ali731-Amb <12252@holbertonstudents.com>
2026-04-30 15:03:44 +0200

    task08

--- ES6_classes/8-hbtn_class.js
+++ ES6_classes/8-hbtn_class.js
@@ -1,4 +1,4 @@
-class HolbertonClass{
+export default class HolbertonClass{
 	constructor(size, location){
 		if (typeof size !== 'number') throw new TypeError('Size must be a number')
 		if (typeof location !== 'string') throw new TypeError('Location must be a string')
