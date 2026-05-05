c5e2ac9f9757741f310f6b3ddff4601924272283
Ali731-Amb <12252@holbertonstudents.com>
2026-05-05 18:19:55 +0200

    Task08

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,5 +1,7 @@
 export default function cleanSet(set, startString){
-	if (startString == ""){ return ""; }
+	if (startString == ""){
+		return ""; 
+		}
 	const arr = [...set]
 	const filtre = arr.filter(val => val.startsWith(startString))
 	const map =filtre.map(val =>val.slice(startString.length))
