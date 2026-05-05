a811a7bd5046a9c4987fcc24e3f7a3bfcd31e939
Ali731-Amb <12252@holbertonstudents.com>
2026-05-05 18:19:21 +0200

    Task06

---
+++ ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,7 @@
+export default function cleanSet(set, startString){
+	if (startString == ""){ return ""; }
+	const arr = [...set]
+	const filtre = arr.filter(val => val.startsWith(startString))
+	const map =filtre.map(val =>val.slice(startString.length))
+	return map.join("-")
+}
