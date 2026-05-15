0d61537c481414c472c2079d2be48d3567940cb7
Ali731-Amb <12252@holbertonstudents.com>
2026-05-15 13:58:39 +0200

    Task05

---
+++ Node_JS_basic/5-http.js
@@ -0,0 +1,24 @@
+const http = require('http');
+const countStudents = require('./3-read_file_async');
+
+const dbPath = process.argv[2];
+
+const app = http.createServer((req, res) => {
+  if (req.url === '/') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello Holberton School!');
+  } else if (req.url === '/students') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    countStudents(dbPath)
+      .then((output) => {
+        res.end(`This is the list of our students\n${output}`);
+      })
+      .catch((err) => {
+        res.end(err.message);
+      });
+  }
+});
+
+app.listen(1245);
+
+module.exports = app;
