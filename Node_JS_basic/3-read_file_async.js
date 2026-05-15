7dd348a8df2694567539b0414c53e79e08e87e19
Ali731-Amb <12252@holbertonstudents.com>
2026-05-15 11:36:33 +0200

    Task03_linterOk

--- Node_JS_basic/3-read_file_async.js
+++ Node_JS_basic/3-read_file_async.js
@@ -1,21 +1,16 @@
 const fs = require('fs');
 
 function countStudents(path) {
-  return new Promise((resolve, reject) => {          // 1. ouvre Promise
-
-    fs.readFile(path, 'utf8', (err, data) => {         // 2. ouvre callback
-
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf8', (err, data) => {
       if (err) {
         reject(new Error('Cannot load the database'));
         return;
       }
-
       const lines = data.split('\n');
       const students = lines.slice(1);
       const newTab = students.filter((line) => line.trim());
-
       console.log(`Number of students: ${newTab.length}`);
-
       const obj = {};
       newTab.forEach((line) => {
         const student = line.split(',');
@@ -25,16 +20,13 @@ function countStudents(path) {
           obj[field] = [];
         }
         obj[field].push(firstname);
-      });                                                    // 3. ferme forEach
-
+      });
       Object.keys(obj).forEach((field) => {
         console.log(`Number of students in ${field}: ${obj[field].length}. List: ${obj[field].join(', ')}`);
-      });                                                    // 4. ferme forEach
-
-      resolve();                                           // ✅ après les forEach
-
-    });                                                      // 5. ferme callback readFile
-  });                                                        // 6. ferme Promise
+      });
+      resolve();
+    });
+  });
 }
 
 module.exports = countStudents;
